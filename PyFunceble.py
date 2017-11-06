e90ac9e6b3c95f036919a097eb9eee0820438771
funilrys <contact@funilrys.com>
2017-11-06 20:44:38 +0100

    Introduction of Helpers.Regex()
    Signed-off-by: funilrys <contact@funilrys.com>

--- PyFunceble.py
+++ PyFunceble.py
@@ -26,6 +26,7 @@ or IP.
 
 import argparse
 from os import path, remove
+from re import compile as comp
 from time import strftime
 
 import requests
@@ -778,6 +779,74 @@ class Helpers(object):
                 remove(self.file)
             except OSError:
                 pass
+
+    class Regex(object):  # pylint: disable=too-few-public-methods
+
+        """A simple implementation ot the python.re package
+
+
+        :param data: A string, the data to regex check
+        :param regex: A string, the regex to match
+        :param return_data: A boolean, if True, return the matched string
+        :param group: A integer, the group to return
+        :param rematch: A boolean, if True, return the matched groups into a
+            formated list. (implementation of Bash ${BASH_REMATCH})
+        """
+
+        def __init__(self, data, regex, **args):
+            super(Regex, self).__init__()
+
+            # We initiate the needed variable in order to be usable all over
+            # class
+            self.data = data
+            self.regex = regex
+
+            # We assign the default value of our optional arguments
+            optional_arguments = {
+                "return_data": True,
+                "group": 0,
+                "rematch": False
+            }
+
+            # We initiate our optional_arguments in order to be usable all over the
+            # class
+            for (arg, default) in optional_arguments.items():
+                setattr(self, arg, args.get(arg, default))
+
+        def match(self):
+            """Used to get exploitable result of re.search"""
+
+            # We initate this variable which gonna contain the returned data
+            result = []
+
+            # We compile the regex string
+            to_match = comp(self.regex)
+
+            # In case we have to use the implementation of ${BASH_REMATCH} we use
+            # re.findall otherwise, we use re.search
+            if self.rematch:  # pylint: disable=no-member
+                pre_result = to_match.findall(self.data)
+            else:
+                pre_result = to_match.search(self.data)
+
+            if self.return_data and pre_result is not None:  # pylint: disable=no-member
+                if self.rematch:  # pylint: disable=no-member
+                    for data in pre_result:
+                        if isinstance(data, tuple):
+                            result.extend(list(data))
+                        else:
+                            result.append(data)
+
+                    if self.group != 0:  # pylint: disable=no-member
+                        return result[self.group]  # pylint: disable=no-member
+                else:
+                    result = pre_result.group(
+                        self.group).strip()  # pylint: disable=no-member
+
+                return result
+            elif not self.return_data and pre_result is not None:  # pylint: disable=no-member
+                return True
+            return False
 
 
 PARSER = argparse.ArgumentParser(
