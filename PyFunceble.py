ea4e1c2084ad390b7f1f638242208ba34ce3df22
funilrys <contact@funilrys.com>
2018-01-24 16:57:48 +0100

    Introduction of `ad` into the list of ignored extensions
    cf: No whois server.

--- PyFunceble.py
+++ PyFunceble.py
@@ -1795,6 +1795,7 @@ class Referer(object):
             '.') + 1:]
 
         self.ignored_extension = [
+            'ad',
             'al',
             'ao',
             'az',
@@ -2583,7 +2584,7 @@ if __name__ == '__main__':
             '-v',
             '--version',
             action='version',
-            version='%(prog)s 0.20.23-beta'
+            version='%(prog)s 0.20.24-beta'
         )
 
         ARGS = PARSER.parse_args()
