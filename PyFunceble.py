fd30a5993e6f092d82852d53a1aef46439ef2333
funilrys <contact@funilrys.com>
2018-01-31 18:58:28 +0100

    Introduction of `bt` into the list of ignored extensions
    cf: Whois lookup to http://www.nic.bt/

--- PyFunceble.py
+++ PyFunceble.py
@@ -1797,6 +1797,7 @@ class Referer(object):
             'bl',
             'bq',
             'bs',
+            'bt',
             'cy',
             'eg',
             'et',
@@ -2667,7 +2668,7 @@ if __name__ == '__main__':
             '-v',
             '--version',
             action='version',
-            version='%(prog)s 0.23.9-beta'
+            version='%(prog)s 0.23.10-beta'
         )
 
         ARGS = PARSER.parse_args()
