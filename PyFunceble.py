3d64ded4b5538820e1cf0c7464de3c76348c48d5
funilrys <contact@funilrys.com>
2018-04-08 19:25:24 +0200

    Introduction of `discover` whois server
    cf: Whois lookup to whois.nic.discover

--- PyFunceble.py
+++ PyFunceble.py
@@ -2664,6 +2664,7 @@ class IANA(object):
             "dealer": "whois.nic.dealer",
             "dell": "whois.nic.dell",
             "dhl": "whois.nic.dhl",
+            "discover": "whois.nic.discover",
             "fj": "whois.usp.ac.fj",
             "ga": "whois.my.ga",
             "gh": "whois.nic.gh",
@@ -3997,7 +3998,7 @@ if __name__ == "__main__":
         help=" Get the latest version of PyFunceble.",
     )
     PARSER.add_argument(
-        "-v", "--version", action="version", version="%(prog)s 0.60.16-beta"
+        "-v", "--version", action="version", version="%(prog)s 0.60.18-beta"
     )
 
     ARGS = PARSER.parse_args()
