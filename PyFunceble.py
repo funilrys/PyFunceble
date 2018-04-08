b41ebf5783ba2c4558b80ee29f30c2c1f7c855c2
funilrys <contact@funilrys.com>
2018-04-08 19:53:00 +0200

    Introduction of `hoteles` whois server
    cf: Whois lookup to whois.nic.hoteles

--- PyFunceble.py
+++ PyFunceble.py
@@ -2703,6 +2703,7 @@ class IANA(object):
             "homegoods": "whois.nic.homegoods",
             "homesense": "whois.nic.homesense",
             "honeywell": "whois.nic.honeywell",
+            "hoteles": "whois.nic.hoteles",
             "hot": "whois.nic.hot",
             "int": "whois.iana.org",
             "kw": "whois.nic.kw",
@@ -4034,7 +4035,7 @@ if __name__ == "__main__":
         help=" Get the latest version of PyFunceble.",
     )
     PARSER.add_argument(
-        "-v", "--version", action="version", version="%(prog)s 0.60.54-beta"
+        "-v", "--version", action="version", version="%(prog)s 0.60.55-beta"
     )
 
     ARGS = PARSER.parse_args()
