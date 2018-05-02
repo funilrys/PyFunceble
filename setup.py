819c0d5a39d983bfe68decdae0173733506c5810
funilrys <contact@funilrys.com>
2018-05-02 12:53:42 +0200

    Introduction of setup.py

---
+++ setup.py
@@ -0,0 +1,120 @@
+"""
+The tool to check domains or IP availability.
+
+
+██████╗ ██╗   ██╗███████╗██╗   ██╗███╗   ██╗ ██████╗███████╗██████╗ ██╗     ███████╗
+██╔══██╗╚██╗ ██╔╝██╔════╝██║   ██║████╗  ██║██╔════╝██╔════╝██╔══██╗██║     ██╔════╝
+██████╔╝ ╚████╔╝ █████╗  ██║   ██║██╔██╗ ██║██║     █████╗  ██████╔╝██║     █████╗
+██╔═══╝   ╚██╔╝  ██╔══╝  ██║   ██║██║╚██╗██║██║     ██╔══╝  ██╔══██╗██║     ██╔══╝
+██║        ██║   ██║     ╚██████╔╝██║ ╚████║╚██████╗███████╗██████╔╝███████╗███████╗
+╚═╝        ╚═╝   ╚═╝      ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝╚══════╝╚═════╝ ╚══════╝╚══════╝
+
+PyFunceble is the little sister of Funceble (https://github.com/funilrys/funceble)
+which was archived on March 13th, 2018.
+At the end of 2017, PyFunceble was described by one of its most active user as:
+"[an] excellent script for checking ACTIVE and INACTIVE domain names."
+
+Our main objective is to test domains and IP availability
+by generating an accurate result based on results from WHOIS, NSLOOKUP and
+HTTP status codes.
+As result, PyFunceble returns 3 status: ACTIVE, INACTIVE and INVALID.
+The denomination of those statuses can be changed under your personal
+`config.yaml`.
+
+At the time we write this, PyFunceble is running actively and daily under 50+
+Travis CI repository or process to test the availability of domains which are
+present into hosts files, AdBlock filter lists, list of IP, list of domains or
+blocklists.
+
+An up to date explanation of all status can be found at https://git.io/vxieo.
+You can also find a simple representation of the logic behind PyFunceble at
+https://git.io/vxifw.
+
+Author:
+    Nissar Chababy, @funilrys, contactTATAfunilrysTODTODcom
+
+Special thanks to:
+    Adam Warner - @PromoFaux
+    Mitchell Krog - @mitchellkrogza
+    Pi-Hole - @pi-hole
+    SMed79 - @SMed79
+
+Contributors:
+    Let's contribute to PyFunceble!!
+
+    Mitchell Krog - @mitchellkrogza
+    Odyseus - @Odyseus
+    WaLLy3K - @WaLLy3K
+    xxcriticxx - @xxcriticxx
+
+    The complete list can be found at https://git.io/vND4m
+
+Original project link:
+    https://github.com/funilrys/PyFunceble
+
+Original project wiki:
+    https://github.com/funilrys/PyFunceble/wiki
+
+License: MIT
+    MIT License
+
+    Copyright (c) 2017-2018 Nissar Chababy
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+"""
+
+from unittest import TestLoader
+
+from setuptools import setup
+
+
+def _test_suite():
+    """
+    This method will discover and run all the tests.
+    """
+
+    test_loader = TestLoader()
+    test_suite = test_loader.discover("tests", pattern="test_*.py")
+    return test_suite
+
+
+setup(
+    name="PyFunceble",
+    version="0.62.0.dev-beta",
+    description="The tool to check domains or IP availability.",
+    long_description=open("README.md").read(),
+    author="funilrys",
+    author_email="contact@funilrys.com",
+    license="MIT https://raw.githubusercontent.com/funilrys/PyFunceble/master/LICENSE",
+    url="https://github.com/funilrys/PyFunceble",
+    platforms=["any"],
+    packages=["PyFunceble"],
+    keywords=["Python", "domain", "IP", "availability", "PyFunceble"],
+    classifiers=[
+        "Environment :: Console",
+        "Topic :: Internet",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    test_suite="setup._test_suite",
+    entry_points={"console_scripts": ["PyFunceble=PyFunceble:command_line"]},
+)
