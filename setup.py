c13ee1a9f670f0630924bcebd982a2703e8ac21b
funilrys <contact@funilrys.com>
2018-05-16 16:48:54 +0200

    Update of version numbers (forgotten)

--- setup.py
+++ setup.py
@@ -107,7 +107,7 @@ def _get_requirements():
 
 setup(
     name="PyFunceble",
-    version="0.65.3",
+    version="0.65.4",
     install_requires=_get_requirements(),
     description="The tool to check domains or IP availability.",
     long_description=open("README.md").read(),
