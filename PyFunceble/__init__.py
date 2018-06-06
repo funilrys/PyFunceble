f77b3659717434d075068b11f8d09c899388c52d
funilrys <contact@funilrys.com>
2018-06-06 19:15:11 +0200

    Update of local branch

--- PyFunceble/__init__.py
+++ PyFunceble/__init__.py
@@ -101,7 +101,7 @@ from PyFunceble.iana import IANA
 from PyFunceble.production import Production
 
 CURRENT_DIRECTORY = getcwd() + directory_separator
-VERSION = "0.75.0.beta"
+VERSION = "0.75.1.dev-beta"
 
 CONFIGURATION_FILENAME = ".PyFunceble.yaml"
 
