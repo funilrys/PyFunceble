11409e266bd35659e43d2996f29f2b6d4ab09ffc
funilrys <contact@funilrys.com>
2018-06-06 09:42:57 +0200

    Update README.md for dev branch

--- PyFunceble/__init__.py
+++ PyFunceble/__init__.py
@@ -101,7 +101,7 @@ from PyFunceble.iana import IANA
 from PyFunceble.production import Production
 
 CURRENT_DIRECTORY = getcwd() + directory_separator
-VERSION = "0.74.2.dev-beta"
+VERSION = "0.74.3.dev-beta"
 
 CONFIGURATION_FILENAME = ".PyFunceble.yaml"
 
