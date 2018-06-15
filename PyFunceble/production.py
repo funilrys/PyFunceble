bde642ba3e77186c221ea0492876ef636118bfd1
funilrys <contact@funilrys.com>
2018-06-16 01:56:50 +0200

    Update of requirements.txt ++  * Update of `--production` logic

--- PyFunceble/production.py
+++ PyFunceble/production.py
@@ -69,63 +69,75 @@ from PyFunceble import Fore, Style
 from PyFunceble.clean import Clean
 from PyFunceble.config import Version
 from PyFunceble.directory_structure import DirectoryStructure
-from PyFunceble.helpers import Dict, File, Regex
+from PyFunceble.helpers import Dict, File, Regex, Command
 
 
 class Production(object):  # pylint: disable=too-few-public-methods
     """
     This class will manage and provide the production logic.
+
+    Argument:
+        - extern: bool
+            True: We do not execute the logic and allow method to be called.
     """
 
-    def __init__(self):
-        self.data_version_yaml = self._get_current_version_yaml()
+    def __init__(self, extern=False):
+        if not extern:
 
-        self.version_yaml = Version(True).split_versions(
-            self.data_version_yaml["current_version"]
-        )
-        self.current_version = Version(True).split_versions(PyFunceble.VERSION, True)
+            if not self._is_dev_version() and not self._is_master_version():
+                raise Exception("Please switch to `dev` or `master` branch.")
 
-        if self._is_version_greater():
-            Clean(None)
-            DirectoryStructure(production=True)
+            self.data_version_yaml = self._get_current_version_yaml()
 
-            if self._does_require_deprecation():
-                to_deprecate = ".".join(self.version_yaml)
+            self.version_yaml = Version(True).split_versions(
+                self.data_version_yaml["current_version"]
+            )
+            self.current_version = Version(True).split_versions(
+                PyFunceble.VERSION, True
+            )
 
-                self.data_version_yaml["deprecated"].append(to_deprecate)
+            if self._is_version_greater():
+                Clean(None)
+                DirectoryStructure(production=True)
 
-            if self._does_require_force_update():
-                to_force_update = ".".join(self.version_yaml)
+                if self._does_require_deprecation():
+                    to_deprecate = ".".join(self.version_yaml)
 
-                self.data_version_yaml["force_update"]["minimal_version"].append(
-                    to_force_update
+                    self.data_version_yaml["deprecated"].append(to_deprecate)
+
+                if self._does_require_force_update():
+                    to_force_update = ".".join(self.version_yaml)
+
+                    self.data_version_yaml["force_update"]["minimal_version"].append(
+                        to_force_update
+                    )
+
+                if self.current_version[-1]:
+                    self.current_version[0].append(self.current_version[-1])
+
+                self.data_version_yaml["current_version"] = ".".join(
+                    self.current_version[0]
                 )
 
-            if self.current_version[-1]:
-                self.current_version[0].append(self.current_version[-1])
+                Dict(self.data_version_yaml).to_yaml(
+                    PyFunceble.CURRENT_DIRECTORY + "version.yaml"
+                )
 
-            self.data_version_yaml["current_version"] = ".".join(
-                self.current_version[0]
-            )
+                self._update_readme_md()
+                self._update_setup_py()
 
-            Dict(self.data_version_yaml).to_yaml(
-                PyFunceble.CURRENT_DIRECTORY + "version.yaml"
-            )
+                message = Fore.GREEN + Style.BRIGHT + "We are ready to ship!! \n"
+                message += Fore.CYAN + "Please do not touch version.yaml nor setup.py (version update)"  # pylint: disable=line-too-long
 
-            self._update_readme_md()
-
-            message = Fore.GREEN + Style.BRIGHT + "We are ready to ship!! \n"
-            message += Fore.CYAN + "Please do not touch version.yaml nor setup.py (version update)"
-
-            print(message)
-            exit(0)
-        else:
-            print(
-                Fore.YELLOW
-                + Style.BRIGHT
-                + "Are you sure that you did some changes ? Please update PyFunceble.VERSION if it is the case."  # pylint: disable=line-too-long
-            )
-            exit(1)
+                print(message)
+                exit(0)
+            else:
+                print(
+                    Fore.YELLOW
+                    + Style.BRIGHT
+                    + "Are you sure that you did some changes ? Please update PyFunceble.VERSION if it is the case."  # pylint: disable=line-too-long
+                )
+                exit(1)
 
     @classmethod
     def _get_current_version_yaml(cls):
@@ -151,6 +163,36 @@ class Production(object):  # pylint: disable=too-few-public-methods
 
         return False
 
+    @classmethod
+    def _is_dev_version(cls):
+        """
+        This method check if the current branch is `dev`.
+        """
+
+        command = "git branch"
+        command_result = Command(command).execute()
+
+        for branch in command_result.split("\n"):
+            if branch.startswith("*") and "dev" in branch:
+                return True
+
+        return False
+
+    @classmethod
+    def _is_master_version(cls):
+        """
+        This method check if the current branch is `master`.
+        """
+
+        command = "git branch"
+        command_result = Command(command).execute()
+
+        for branch in command_result.split("\n"):
+            if branch.startswith("*") and "master" in branch:
+                return True
+
+        return False
+
     def _does_require_deprecation(self):
         """
         This method check if we have to put the previous version into the deprecated list.
@@ -173,8 +215,7 @@ class Production(object):  # pylint: disable=too-few-public-methods
 
         return False
 
-    @classmethod
-    def _update_readme_md(cls):
+    def _update_readme_md(self):
         """
         This method update README.md so that it's always giving branch related bases.
         Note: This only apply to dev and master
@@ -182,10 +223,12 @@ class Production(object):  # pylint: disable=too-few-public-methods
 
         readme_path = PyFunceble.CURRENT_DIRECTORY + "README.md"
 
-        if "dev" in PyFunceble.VERSION:
+        if self._is_dev_version():
             regexes = {"/dev/": r"\/master\/", "=dev": "=master"}
-        else:
+        elif self._is_master_version():
             regexes = {"/master/": r"\/dev\/", "=master": "=dev"}
+        else:
+            raise Exception("Please switch to `dev` or `master` branch.")
 
         to_update = File(readme_path).read()
 
@@ -193,3 +236,30 @@ class Production(object):  # pylint: disable=too-few-public-methods
             to_update = Regex(to_update, regex, replace_with=replacement).replace()
 
         File(readme_path).write(to_update, overwrite=True)
+
+    def _update_setup_py(self):
+        """
+        This method will update setup.py so that it always have the right name.
+        """
+
+        setup_py_path = PyFunceble.CURRENT_DIRECTORY + "setup.py"
+
+        if self._is_dev_version():
+            regexes = {
+                'name="PyFunceble-dev"': r'name=".*"',
+                "Development Status :: 4 - Beta": r'Development\sStatus\s::.*"',
+            }
+        elif self._is_master_version():
+            regexes = {
+                'name="PyFunceble"': r'name=".*"',
+                "Development Status :: 5 - Production/Stable": r'Development\sStatus\s::.*"',
+            }
+        else:
+            raise Exception("Please switch to `dev` or `master` branch.")
+
+        to_update = File(setup_py_path).read()
+
+        for replacement, regex in regexes.items():
+            to_update = Regex(to_update, regex, replace_with=replacement).replace()
+
+        File(setup_py_path).write(to_update, overwrite=True)
