4074ca6b33a7206ac669e94fe443e536cf5b3fcf
funilrys <contact@funilrys.com>
2018-07-22 00:49:06 +0200

    Update of the links in the help

--- PyFunceble/__init__.py
+++ PyFunceble/__init__.py
@@ -75,7 +75,7 @@ from PyFunceble.production import Production
 from PyFunceble.publicsuffix import PublicSuffix
 
 CURRENT_DIRECTORY = getcwd() + directory_separator
-VERSION = "0.84.3.dev-beta"
+VERSION = "0.84.4.dev-beta"
 
 CONFIGURATION_FILENAME = ".PyFunceble.yaml"
 
@@ -148,12 +148,12 @@ def _command_line():  # pragma: no cover  # pylint: disable=too-many-branches,to
                 + "with the help of "
                 + Style.BRIGHT
                 + Fore.GREEN
-                + "https://git.io/vND4m "
+                + "https://git.io/fNWb9"
                 + Style.RESET_ALL
                 + "&& "
                 + Style.BRIGHT
                 + Fore.GREEN
-                + "https://git.io/vND4a",
+                + "https://git.io/fNWbH",
             ),
             add_help=False,
         )
