753da583ee9bf01718be463e517ead43cd39b01d
funilrys <contact@funilrys.com>
2018-08-29 18:30:30 +0200

    Introduction of more inline comments

--- PyFunceble/clean.py
+++ PyFunceble/clean.py
@@ -81,7 +81,12 @@ class Clean:
 
     def __init__(self, list_to_test):
         if list_to_test:
+            # The list to test is not empty.
+
             try:
+                # We try to see if we have to reset counters and clean the output directory.
+
+                # We get the number of tested.
                 number_of_tested = PyFunceble.CONFIGURATION["counter"]["number"][
                     "tested"
                 ]
@@ -91,14 +96,31 @@ class Clean:
                     or list_to_test[number_of_tested - 1] == list_to_test[-1]
                     or number_of_tested == len(list_to_test)
                 ):
+                    # * If the number of tested is null,
+                    # or
+                    # * the last tested element is the same as the last element in the
+                    #   sequence,
+                    # or
+                    # * The number of tested is equal to the number of elements in the
+                    #   sequence,
+
+                    # We reset the counters.
                     Core.reset_counters()
 
+                    # We clean the output directory.
                     self.all()
             except IndexError:
+                # But if at any time in the conditionnal an Index Error occurs,
+
+                # We reset the counters.
                 Core.reset_counters()
 
+                # We clean the output directory.
                 self.all()
         else:
+            # The list to test is empty.
+
+            # We clean the output directory.
             self.all()
 
     @classmethod
@@ -107,23 +129,43 @@ class Clean:
         Return the list of file to delete.
         """
 
+        # We initiate the directory we have to look for.
         directory = PyFunceble.OUTPUT_DIRECTORY + PyFunceble.OUTPUTS["parent_directory"]
 
         if not directory.endswith(directory_separator):  # pragma: no cover
+            # For safety, if it does not ends with the directory separator, we append it
+            # to its end.
             directory += directory_separator
 
+        # We initiate a variable which will save the list of file to delete.
         result = []
 
         for root, _, files in walk(directory):
+            # We walk in the directory and get all files and sub-directories.
+
             for file in files:
+                # If there is files in the current sub-directory, we loop
+                # through the list of files.
+
                 if file not in [".gitignore", ".keep"]:
+                    # The file is not into our list of file we do not have to delete.
+
                     if root.endswith(directory_separator):
+                        # The root ends with the directory separator.
+
+                        # We construct the path and append the full path to the result.
                         result.append(root + file)
                     else:
+                        # The root directory does not ends with the directory separator.
+
+                        # We construct the path by appending the directory separator
+                        # between the root and the filename and append the full path to
+                        # the result.
                         result.append(
                             root + directory_separator + file
                         )  # pragma: no cover
 
+        # We return our list of file to delete.
         return result
 
     def all(self):
@@ -131,7 +173,11 @@ class Clean:
         Delete all discovered files.
         """
 
+        # We get the list of file to delete.
         to_delete = self.file_to_delete()
 
         for file in to_delete:
+            # We loop through the list of file to delete.
+
+            # And we delete the currently read file.
             File(file).delete()
