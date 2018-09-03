d5064e9c0f2233936ca591205f04e648e39d13ef
funilrys <contact@funilrys.com>
2018-09-03 22:22:38 +0200

    Introduction of more inline comments

--- PyFunceble/prints.py
+++ PyFunceble/prints.py
@@ -86,13 +86,21 @@ class Prints:
     """
 
     def __init__(self, to_print, template, output_file=None, only_on_file=False):
+        # We get the template.
         self.template = template
+        # We get the output.
         self.output = output_file
+        # We get the data to print.
         self.data_to_print = to_print
+        # We get the state of the only on file.
         self.only_on_file = only_on_file
 
+        # We initiate the variable which will save the list of header.
+        # Note: We initiate an Ordered Dict because we want to keep
+        # the order.
         self.headers = OrderedDict()
 
+        # We iniate the Generic header and the spacement of each colomns.
         self.headers["Generic"] = OrderedDict(
             zip(
                 [
@@ -107,6 +115,7 @@ class Prints:
             )
         )
 
+        # We iniate the official UP header and the spacement of each colomns.
         self.headers[PyFunceble.STATUS["official"]["up"]] = OrderedDict(
             zip(
                 ["Domain", "Expiration Date", "Source", "HTTP Code", "Analyze Date"],
@@ -114,6 +123,7 @@ class Prints:
             )
         )
 
+        # We iniate the official DOWN header and the spacement of each colomns.
         self.headers[PyFunceble.STATUS["official"]["down"]] = OrderedDict(
             zip(
                 [
@@ -128,22 +138,27 @@ class Prints:
             )
         )
 
+        # We iniate the official INVALID header and the spacement of each colomns.
         self.headers[PyFunceble.STATUS["official"]["invalid"]] = OrderedDict(
             zip(["Domain", "Source", "HTTP Code", "Analyze Date"], [100, 10, 10, 20])
         )
 
+        # We iniate the official LESS header and the spacement of each colomns.
         self.headers["Less"] = OrderedDict(
             zip(["Domain", "Status", "HTTP Code"], [100, 11, 10])
         )
 
+        # We iniate the official Percentage header and the spacement of each colomns.
         self.headers["Percentage"] = OrderedDict(
             zip(["Status", "Percentage", "Numbers"], [11, 12, 12])
         )
 
+        # We iniate the official HTTP header and the spacement of each colomns.
         self.headers["HTTP"] = OrderedDict(
             zip(["Domain", "Status", "HTTP Code", "Analyze Date"], [100, 11, 10, 20])
         )
 
+        # We initiate a variable which will save the currently in use header.
         self.currently_used_header = {}
 
     def _before_header(self):
@@ -157,25 +172,47 @@ class Prints:
             and self.output
             and not path.isfile(self.output)
         ):
+            # * We are allowed to generate files.
+            # and
+            # * And output is given.
+            # and
+            # * The given output does not exist.
+
+            # We initiate the information about what generated the file.
             link = "# File generated with %s\n" % PyFunceble.LINKS["repo"]
+
+            # We initiate the information about the generation date of this file.
             date_of_generation = (
                 "# Date of generation: %s \n\n" % PyFunceble.CURRENT_TIME
             )
 
-            if self.template in [
+            # We initiate a variable which will save the list of
+            # templates which have to meet in order to write the before
+            # header informations.
+            authorized_templates = [
                 "Generic_File",
                 PyFunceble.STATUS["official"]["up"],
                 PyFunceble.STATUS["official"]["down"],
                 PyFunceble.STATUS["official"]["invalid"],
                 "Less",
-            ]:
+            ]
+
+            if self.template in authorized_templates:
+                # The current header is in our list of authorized templated.
+
+                # We get the header.
                 header = (
                     self._header_constructor(self.currently_used_header, None)[0] + "\n"
                 )
 
             try:
+                # We try to print the link, the date of generation and the header in the
+                # given file.
                 File(self.output).write(link + date_of_generation + header)
             except UnboundLocalError:
+                # We don't have any header.
+
+                # We print the link and the date in the given file.
                 File(self.output).write(link + date_of_generation)
 
     @classmethod
@@ -197,37 +234,74 @@ class Prints:
             The data to print in list format.
         """
 
+        # We initiate a variable which will save the header data.
         header_data = []
+
+        # We initiate a variable which will save the header sizes.
         header_size = ""
+
+        # We initiate the glue to set before the size.
         before_size = "%-"
+
+        # We initiate the glue to set after the size.
         after_size = "s"
 
         if header_separator:
+            # The header separator is not empty.
+
+            # We initiate a variable which will save the list of
+            # separator data.
             header_separator_data = []
 
+        # We get the length of the data to print.
         length_data_to_print = len(data_to_print) - 1
+
+        # We initiate an iterator.
         i = 0
 
         for data in data_to_print:
+            # We loop through the list of data.
+
+            # We get the size of the currently read data.
             size = data_to_print[data]
+
+            # We append the data to the header data list.
             header_data.append(data)
 
+            # We construct the header size.
+            # Note: our header size is formated line %s-sizes
+            # (the s at the end is part of the formatting.)
             header_size += before_size + str(size) + after_size
 
             if i < length_data_to_print:
+                # The iterator is less than the length of data to print.
+
+                # We append the the colomn separator to the header size.
                 header_size += column_separator
 
             if header_separator:
+                # The header separator is given.
+
+                # We append the right size of separator to the list of
+                # separator data.
                 header_separator_data.append(header_separator * size)
 
+            # We increase the iterator.
             i += 1
 
         if header_separator:
+            # The header separator is given.
+
             return [
+                # We return the formated header (like we will do with print('%s' % 'hello'))
                 header_size % tuple(header_data),
+                # We return the formated header separator.
                 header_size % tuple(header_separator_data),
             ]
 
+        # The header separator is not given.
+
+        # We return the formetted header.
         return [header_size % tuple(header_data)]
 
     def header(
@@ -236,6 +310,12 @@ class Prints:
         """
         Management and creation of templates of header.
         Please consider as "header" the title of each columns.
+
+        Argument:
+            - do_not_print: bool
+                True: We don't print anything.
+                False: We print what we want.
+
         """
 
         if (
@@ -243,44 +323,98 @@ class Prints:
             or self.template == "Percentage"
             or do_not_print
         ):
+            # * The header has not been already printed.
+            # or
+            # * The template is the `Percentage template`.
+            # or
+            # * We are authorized to print something.
+
             if (
                 self.template.lower() in PyFunceble.STATUS["list"]["generic"]
                 or self.template == "Generic_File"
             ):
+                # * The template is into the list of generic status.
+                # or
+                # * The template is equal to `Generic_File`.
+
+                # The data to print is the Generic header.
                 to_print = self.headers["Generic"]
 
                 if (
                     self.template.lower() in PyFunceble.STATUS["list"]["generic"]
                     and PyFunceble.HTTP_CODE["active"]
                 ):
+                    # * The template is in the list of generic status.
+                    # and
+                    # * the http status code extraction is activated.
+
+                    # We remove the Analyze Date colomn from the data to print.
                     to_print = Dict(to_print).remove_key("Analyze Date")
             if self.template.lower() in PyFunceble.STATUS["list"]["up"]:
+                # The template is in the list of up status.
+
+                # We informations to print is the up header.
                 to_print = self.headers[PyFunceble.STATUS["official"]["up"]]
             elif self.template.lower() in PyFunceble.STATUS["list"]["down"]:
+                # The template is in the list of down status.
+
+                # We informations to print is the down header.
                 to_print = self.headers[PyFunceble.STATUS["official"]["down"]]
             elif self.template.lower() in PyFunceble.STATUS["list"]["invalid"]:
+                # The template is in the list of invalid status.
+
+                # We informations to print is the invalid header.
                 to_print = self.headers[PyFunceble.STATUS["official"]["invalid"]]
             elif (
                 self.template == "Less"
                 or self.template == "Percentage"
                 or self.template == "HTTP"
             ):  # pylint: disable=line-too-long
+                # * The template is equal to `Less`.
+                # or
+                # * The template is equal to `Percentage`.
+                # or
+                # * The template is equal to `HTTP`.
+
+                # We get the header with the help of the template name.
                 to_print = self.headers[self.template]
 
                 if self.template == "Less" and not PyFunceble.HTTP_CODE["active"]:
+                    # * The template is equal to `Less`.
+                    # and
+                    # * The http status code extraction is deactivated.
+
+                    # We append the source index to the header.
                     to_print["Source"] = 10
 
             if not PyFunceble.HTTP_CODE["active"]:
+                # * The http status code extraction is deactivated.
+
+                # We remove the HTTP Code index from the data to print.
                 to_print = Dict(to_print).remove_key("HTTP Code")
 
+            # We update the currently used header.
             self.currently_used_header = to_print
 
             if not do_not_print:
+                # We are not authorized to print anything.
+
+                # We generate the before header.
                 self._before_header()
+
                 for formated_template in self._header_constructor(to_print):
+                    # We loop through the formated template.
+
                     if not self.only_on_file:
+                        # We do not have to print only on file.
+
+                        # We print on screen the formated header template.
                         print(formated_template)
-                    if self.output:
+
+                    if not PyFunceble.CONFIGURATION["no_files"] and self.output:
+                        # An output destination is given.
+
+                        # We write the file with the formated header template.
                         File(self.output).write(formated_template + "\n")
 
     def _data_constructor(self, size):
@@ -299,9 +433,18 @@ class Prints:
             - Exception: if the data and the size does not have the same length.
         """
 
+        # We initiate a variable which will save what we are going to
+        # return.
         result = OrderedDict()
+
         if len(self.data_to_print) == len(size):
+            # The length of the data to print is equal to the length of the given size.
+
             for i in range(len(self.data_to_print)):
+                # We loop until our iterator is less or equal to the length of the data
+                # to print.
+
+                # We initiate the result index and its size.
                 result[self.data_to_print[i]] = size[i]
         else:
             # This should never happend. If it's happens then there is something
@@ -310,6 +453,7 @@ class Prints:
                 "Inputed: " + str(len(self.data_to_print)) + "; Size: " + str(len(size))
             )
 
+        # We return the constructed result.
         return result
 
     @classmethod
@@ -325,11 +469,16 @@ class Prints:
             The maximal size of the each data to print.
         """
 
+        # We initiate the result we are going to return.
         result = []
 
         for data in header:
+            # We lopp through the header.
+
+            # And we append the size to our result.
             result.append(header[data])
 
+        # We return the result.
         return result
 
     def _colorify(self, data):
@@ -345,12 +494,25 @@ class Prints:
         """
 
         if self.template in ["Generic", "Less"]:
+            # The template is in the list of template that need the coloration.
+
             if self.data_to_print[1].lower() in PyFunceble.STATUS["list"]["up"]:
+                # The status is in the list of up status.
+
+                # We print the data with a green background.
                 data = Fore.BLACK + Back.GREEN + data
             elif self.data_to_print[1].lower() in PyFunceble.STATUS["list"]["down"]:
+                # The status is in the list of down status.
+
+                # We print the data with a red background.
                 data = Fore.BLACK + Back.RED + data
             else:
+                # The status is not in the list of up and down status.
+
+                # We print the data with a cyan background.
                 data = Fore.BLACK + Back.CYAN + data
+
+        # We return the data.
         return data
 
     def data(self):  # pragma: no cover
@@ -362,33 +524,75 @@ class Prints:
         """
 
         if isinstance(self.data_to_print, list):
+            # The data to print is a list.
+
+            # We initiate the data we are going to print.
             to_print = {}
+
+            # We initiate the size we are going to print.
             to_print_size = []
 
+            # We initiate a variable which will list the list of
+            # alone case.
             alone_cases = ["Percentage", "HTTP"]
+
+            # we initiate a variable which will list the list of
+            # template which does not need a header.
             without_header = ["FullHosts", "PlainDomain"]
 
             if self.template not in alone_cases and self.template not in without_header:
+                # * The template is not in the list of alone case.
+                # and
+                # * THe template is not in the list of template without header.
+
+                # We get the template we should use.
+                # Note: We basically only need the self.currently_used_header to be filled.
                 self.header(True)
+
+                # And we get the size from the header.
                 to_print_size = self._size_from_header(self.currently_used_header)
             elif self.template in without_header:
+                # The template is in the list of template which does not need a header.
+
                 for data in self.data_to_print:
+                    # We loop through the list of data to print.
+
+                    # And we construct the (spacement) size of the data to print.
                     to_print_size.append(str(len(data)))
             else:
+                # We get the size from the given template name.
                 to_print_size = self._size_from_header(self.headers[self.template])
 
+            # We construct and format the data to print.
             to_print = self._data_constructor(to_print_size)
 
+            # We print the before header section.
             self._before_header()
 
             for data in self._header_constructor(to_print, False):
+                # We loop through the formated data.
+
                 if self.template.lower() in PyFunceble.STATUS["list"][
                     "generic"
                 ] or self.template in ["Less", "Percentage"]:
+                    # * The template is in the list of generic status.
+                    # or
+                    # * The template is in a specific list.
+
                     if not self.only_on_file:
-                        data = self._colorify(data)
-                        print(data)
+                        # We are authorized to print on screen.
+
+                        # We colorify the data to print.
+                        colorified_data = self._colorify(data)
+
+                        # And we print the data.
+                        print(colorified_data)
                 if not PyFunceble.CONFIGURATION["no_files"] and self.output:
+                    # * We are authorized to print on any file.
+                    # and
+                    # * The output is given.
+
+                    # We write our data into the printed file.
                     File(self.output).write(data + "\n")
         else:
             # This should never happend. If it's happens then there's a big issue
