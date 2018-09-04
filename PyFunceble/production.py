d1861d3d718218069181c569810add92a6ab988a
funilrys <contact@funilrys.com>
2018-09-04 21:21:31 +0200

    Introduction of more inline comments

--- PyFunceble/production.py
+++ PyFunceble/production.py
@@ -84,47 +84,78 @@ class Production:  # pylint: disable=too-few-public-methods
 
     def __init__(self, extern=False):
         if not extern:
+            # A method of this class is not called.
 
             if not self.is_dev_version() and not self.is_master_version():
+                # The version is not the `dev` version nor the `master` version.
+
+                # We raise an exception telling the user that there is a
+                # problem somewhere around the versioning.
                 raise Exception("Please switch to `dev` or `master` branch.")
 
+            # We read and get the current content of `version.yaml`.
             self.data_version_yaml = self._get_current_version_yaml()
 
+            # We split the version in oder to get only the list of digits from
+            # the local version.
             self.version_yaml = Version(True).split_versions(
                 self.data_version_yaml["current_version"]
             )
+
+            # We we get the full version with the non-digits and the digits.
             self.current_version = Version(True).split_versions(
                 PyFunceble.VERSION, True
             )
 
             if self._is_version_greater():
+                # The local version is greater than the older one.
+
+                # We clean the output directory.
                 Clean(None)
+
+                # We generate the productive directory structure file.
                 DirectoryStructure(production=True)
 
                 if self._does_require_deprecation():
+                    # We have to put the previous version into the list of deprecated list.
+
+                    # We reconstruct the version.
                     to_deprecate = ".".join(self.version_yaml)
 
+                    # And we append it into the list of deprecated version.
                     self.data_version_yaml["deprecated"].append(to_deprecate)
 
                 if self._does_require_force_update():
+                    # We have to put the previous version into the list of forced for update list.
+
+                    # We reconstruct the version.
                     to_force_update = ".".join(self.version_yaml)
 
+                    # And we append it into the list of minimal version.
                     self.data_version_yaml["force_update"]["minimal_version"].append(
                         to_force_update
                     )
 
                 if self.current_version[-1]:
+                    # The non digit part of the version is not empty.
+
+                    # We append it to the digit part.
                     self.current_version[0].append(self.current_version[-1])
 
+                # We update the current version.
                 self.data_version_yaml["current_version"] = ".".join(
                     self.current_version[0]
                 )
 
+                # We fix the urls in the README file.
                 self._update_docs(PyFunceble.CURRENT_DIRECTORY + "README.rst")
+
+                # We fix the urls in the configuration file.
                 self._update_docs(
                     PyFunceble.CURRENT_DIRECTORY + ".PyFunceble_production.yaml"
                 )
 
+                # We fix the urls in the documentation index.
                 self._update_docs(
                     PyFunceble.CURRENT_DIRECTORY
                     + directory_separator
@@ -133,29 +164,43 @@ class Production:  # pylint: disable=too-few-public-methods
                     + "index.rst"
                 )
 
+                # We fix the urls in the code.
                 self._update_code_urls()
 
+                # We fix the setup.py.
                 self._update_setup_py()
+
+                # We fix the .travis.yml file.
                 self._update_travis_yml()
 
+                # We save our version data into our `version.yaml` file.
                 Dict(self.data_version_yaml).to_yaml(
                     PyFunceble.CURRENT_DIRECTORY + "version.yaml"
                 )
 
+                # We prepare the message we are going to print on screen.
                 message = Fore.GREEN + Style.BRIGHT + "We are ready to ship!! \n"
                 message += (
                     Fore.CYAN
                     + "Please do not touch version.yaml nor setup.py (version update)"
                 )  # pylint: disable=line-too-long
 
+                # We print the message.
                 print(message)
+
+                # We exit the process.
                 exit(0)
             else:
+                # The local version is less or equal to the older one.
+
+                # We print a message on screen.
                 print(
                     Fore.YELLOW
                     + Style.BRIGHT
                     + "Are you sure that you did some changes ? Please update PyFunceble.VERSION if it is the case."  # pylint: disable=line-too-long
                 )
+
+                # We exit the process.
                 exit(1)
 
     def _update_code_urls(self):
@@ -163,22 +208,34 @@ class Production:  # pylint: disable=too-few-public-methods
         This method will read the code and update all links.
         """
 
+        to_ignore = [".gitignore", ".keep", "production.py", "publicsuffix.py"]
+
         for root, _, files in walk(
             PyFunceble.CURRENT_DIRECTORY
             + directory_separator
             + "PyFunceble"
             + directory_separator
         ):
-            for file in files:
-                if (
-                    file
-                    not in [".gitignore", ".keep", "production.py", "publicsuffix.py"]
-                    and "__pycache__" not in root
-                ):
-                    if root.endswith(directory_separator):
+            # We loop through every directories and files in the `PyFunceble` directory.
 
+            for file in files:
+                # We loop through the list of files of the currently read directory.
+
+                if file not in to_ignore and "__pycache__" not in root:
+                    # * The filename is not into the list of file to ignore.
+                    # and
+                    # * The directory we are reading is not `__pycache__`.
+
+                    if root.endswith(directory_separator):
+                        # The root directory ends with the directory separator.
+
+                        # We fix the path in the currently read file.
                         self._update_docs(root + file)
                     else:
+                        # The root directory does not ends with the directory separator.
+
+                        # We fix the path in the currently read file.
+                        # (after appending the directory separator between the root and file)
                         self._update_docs(root + directory_separator + file)
 
         for root, _, files in walk(
@@ -187,12 +244,25 @@ class Production:  # pylint: disable=too-few-public-methods
             + "tests"
             + directory_separator
         ):
+            # We loop through every directories and files in the `tests` directory.
             for file in files:
-                if file not in [".gitignore", ".keep"] and "__pycache__" not in root:
-                    if root.endswith(directory_separator):
+                # We loop through the list of files of the currently read directory.
 
+                if file not in to_ignore and "__pycache__" not in root:
+                    # * The filename is not into the list of file to ignore.
+                    # and
+                    # * The directory we are reading is not `__pycache__`.
+
+                    if root.endswith(directory_separator):
+                        # The root directory ends with the directory separator.
+
+                        # We fix the path in the currently read file.
                         self._update_docs(root + file)
                     else:
+                        # The root directory does not ends with the directory separator.
+
+                        # We fix the path in the currently read file.
+                        # (after appending the directory separator between the root and file)
                         self._update_docs(root + directory_separator + file)
 
     @classmethod
@@ -210,13 +280,18 @@ class Production:  # pylint: disable=too-few-public-methods
         This method check if the current version is greater as the older older one.
         """
 
+        # we compare the 2 versions.
         checked = Version(True).check_versions(
             self.current_version[0], self.version_yaml
         )
 
         if checked is not None and not checked:
+            # The current version is greater as the older one.
+
+            # We return True.
             return True
 
+        # We return False
         return False
 
     @classmethod
@@ -225,13 +300,25 @@ class Production:  # pylint: disable=too-few-public-methods
         This method check if the current branch is `dev`.
         """
 
+        # We initiate the command we have to run in order to
+        # get the branch we are currently working with.
         command = "git branch"
+
+        # We execute and get the command output.
         command_result = Command(command).execute()
 
         for branch in command_result.split("\n"):
+            # We loop through each line of the command output.
+
             if branch.startswith("*") and "dev" in branch:
+                # The current branch is `dev`.
+
+                # We return True.
                 return True
 
+        # The current branch is not `dev`.
+
+        # We return False.
         return False
 
     @classmethod
@@ -240,13 +327,25 @@ class Production:  # pylint: disable=too-few-public-methods
         This method check if the current branch is `master`.
         """
 
+        # We initiate the command we have to run in order to
+        # get the branch we are currently working with.
         command = "git branch"
+
+        # We execute and get the command output.
         command_result = Command(command).execute()
 
         for branch in command_result.split("\n"):
+            # We loop through each line of the command output.
+
             if branch.startswith("*") and "master" in branch:
+                # The current branch is `master`.
+
+                # We return True.
                 return True
 
+        # The current branch is not `master`.
+
+        # We return False.
         return False
 
     def _does_require_deprecation(self):
@@ -255,9 +354,16 @@ class Production:  # pylint: disable=too-few-public-methods
         """
 
         for index, version_number in enumerate(self.current_version[0][:2]):
+            # We loop through the 2 last elements of the version.
+
             if version_number > self.version_yaml[index]:
+                # The currently read version number is greater than the one we have in
+                # the version.yaml.
+
+                # We return True.
                 return True
 
+        # We return False, we do not need to deprecate anything.
         return False
 
     def _does_require_force_update(self):
@@ -267,8 +373,14 @@ class Production:  # pylint: disable=too-few-public-methods
         """
 
         if self.current_version[0][0] > self.version_yaml[0]:
+            # The current version first index is greater than the one we have in the
+            # current version.yaml.
+
+            # We return True.
             return True
 
+        # We return False, we do not need to force the update for
+        # the current version number.
         return False
 
     def _update_docs(self, file_to_update):
@@ -282,17 +394,34 @@ class Production:  # pylint: disable=too-few-public-methods
         """
 
         if self.is_dev_version():
+            # The current version is the dev version.
+
+            # We map what we have to replace.
+            # Format: {match:replacement}
             regexes = {"/dev/": r"\/master\/", "=dev": "=master"}
         elif self.is_master_version():
+            # The current version is the master version.
+
+            # We map what we have to replace.
             regexes = {"/master/": r"\/dev\/", "=master": "=dev"}
         else:
+            # The current version is not the master nor the dev version.
+
+            # We raise an exception as the branch we are currently is not meaned
+            # for production.
             raise Exception("Please switch to `dev` or `master` branch.")
 
+        # We get the content of the file to fix.
         to_update = File(file_to_update).read()
 
         for replacement, regex in regexes.items():
+            # We loop through reach element of the map.
+
+            # We process the replacement.
             to_update = Regex(to_update, regex, replace_with=replacement).replace()
 
+        # We finally overwrite the file to fix with the filtered.
+        # content.
         File(file_to_update).write(to_update, overwrite=True)
 
     def _update_setup_py(self):
@@ -300,26 +429,44 @@ class Production:  # pylint: disable=too-few-public-methods
         This method will update setup.py so that it always have the right name.
         """
 
+        # We initiate the path to the file we have to filter.
         setup_py_path = PyFunceble.CURRENT_DIRECTORY + "setup.py"
 
         if self.is_dev_version():
+            # The current version is the `dev` version.
+
+            # We map what we have to replace.
+            # Format: {match:replacement}
             regexes = {
                 'name="PyFunceble-dev"': r'name=".*"',
                 '"Development Status :: 4 - Beta"': r'"Development\sStatus\s::.*"',
             }
         elif self.is_master_version():
+            # The current version is the `dev` version.
+
+            # We map what we have to replace.
             regexes = {
                 'name="PyFunceble"': r'name=".*"',
                 '"Development Status :: 5 - Production/Stable"': r'"Development\sStatus\s::.*"',
             }
         else:
+            # The current version is not the `dev` nor the `master` version.
+
+            # We raise an exception to the user, the current branch is not meant for
+            # production.
             raise Exception("Please switch to `dev` or `master` branch.")
 
+        # We get the file content.
         to_update = File(setup_py_path).read()
 
         for replacement, regex in regexes.items():
+            # We loop through our map.
+
+            # And we process the replacement.
             to_update = Regex(to_update, regex, replace_with=replacement).replace()
 
+        # We finally replace the content of the file with the filtered
+        # version.
         File(setup_py_path).write(to_update, overwrite=True)
 
     def _update_travis_yml(self):
@@ -327,24 +474,41 @@ class Production:  # pylint: disable=too-few-public-methods
         This method will update .travis.yml according to current branch.
         """
 
+        # We initiate the file we have to filter/update.
         travis_yml_path = PyFunceble.CURRENT_DIRECTORY + ".travis.yml"
 
         if self.is_dev_version():
+            # The current version is the `dev` version.
+
+            # We map what we have to replace.
+            # Format: {match:replacement}
             regexes = {
                 "pip3 install pyfunceble-dev": r"pip3\sinstall\spyfunceble.*",
                 "pip-autoremove pyfunceble-dev ": r"pip-autoremove\spyfunceble\s",
             }
         elif self.is_master_version():
+            # The current version is the `master` version.
+
+            # We map what we have to replace.
             regexes = {
                 "pip3 install pyfunceble": r"pip3\sinstall\spyfunceble.*",
                 "pip-autoremove pyfunceble ": r"pip-autoremove\spyfunceble[a-z-_]+\s",
             }
         else:
+            # The current version is not the `master` nor the `dev` version.
+
+            # We raise an exception, the current branch is not meant for production.
             raise Exception("Please switch to `dev` or `master` branch.")
 
+        # We get the file content.
         to_update = File(travis_yml_path).read()
 
         for replacement, regex in regexes.items():
+            # We loop through the map.
+
+            # And we process the replacement.
             to_update = Regex(to_update, regex, replace_with=replacement).replace()
 
+        # We finaly replace the file content with the filtered
+        # content.
         File(travis_yml_path).write(to_update, overwrite=True)
