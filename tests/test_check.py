e57be306c1aaa8d558c3e06a82e6c2c414316374
funilrys <contact@funilrys.com>
2018-11-01 08:50:46 +0100

    Improvement of the check tests

--- tests/test_check.py
+++ tests/test_check.py
@@ -65,40 +65,107 @@ from unittest import TestCase
 from unittest import main as launch_tests
 
 import PyFunceble
+from PyFunceble import load_config
 from PyFunceble.check import Check
 
 
 class TestCheck(TestCase):
     """
-    This class will test PyFunceble.check.Check().
+    Test PyFunceble.check.Check().
     """
+
+    def setUp(self):
+        """
+        Setup what we need for the tests.
+        """
+
+        load_config(True)
+
+        self.valid_domain = [
+            "_hello_.abuse.co.za",
+            "_hello_world_.abuse.co.za",
+            "_hello_world_.hello.eu.com",
+            "_hello-beautiful-world_.wold.eu.com",
+            "_hello-world.abuse.co.za",
+            "_hello._world.abuse.co.za",
+            "_hello.abuse.co.za",
+            "_world_.hello.eu.com",
+            "_world.hello.eu.com",
+            "hello_.world.eu.com",
+            "hello_world.abuse.co.za",
+            "hello_world.world.com",
+            "hello_world.world.hello.com",
+            "hello---world.com",
+            "hello-.abuse.co.za",
+            "hello-world.com",
+            "hello.world_hello.world.com",
+            "hello.world.com",
+            "hello.world.hello.com",
+            "xn--bittr-fsa6124c.com",
+            "xn--bllogram-g80d.com",
+            "xn--coinbse-30c.com",
+            "xn--cryptopi-ux0d.com",
+            "xn--cyptopia-4e0d.com",
+        ]
+
+        self.not_valid_domain = [
+            "_world._hello.eu.com",
+            "_world.hello_.eu.com",
+            "-hello-.abuse.co.za",
+            "-hello-world_.abuse.co.za",
+            "-hello-world_all-mine_.hello.eu.com",
+            "-hello.abuse.co.za",
+            "-hello.world",
+            "-world.hello",
+            "bịllogram.com",
+            "bittréẋ.com",
+            "coinbȧse.com",
+            "cryptopiạ.com",
+            "cṙyptopia.com",
+            "hello_world_.com",
+            "hello_world.com",
+            "hello-.world",
+            "hello-world",
+            "hello.-hello-world_.abuse.co.za",
+            "hello.com:443",
+            "hello@world.com",
+            "httpWd",
+            "test.-hello-world_all-mine_.abuse.co.za",
+            "world_hello.com",
+            "world-.hello",
+            "world-hello",
+            "world.hello:80",
+            "world@hello.com",
+        ]
 
     def test_is_url_valid(self):
         """
-        This method will test URL.is_url_valid().
+        Test URL.is_url_valid() for the case that the URL is valid.
         """
 
-        # Test of the case that the URL is valid
         expected = True
-        PyFunceble.CONFIGURATION["to_test"] = "http://hello.world/helloworld"
-        actual = Check().is_url_valid()
 
-        self.assertEqual(expected, actual)
-        actual = Check(PyFunceble.CONFIGURATION["to_test"]).is_url_valid()
-        self.assertEqual(expected, actual)
+        for domain in self.valid_domain:
+            PyFunceble.CONFIGURATION["to_test"] = "http://%s/helloworld" % domain
 
-        # Test of the case that the domains is not valid
-        not_valid = [
-            "world-hello",
-            "-world.hello",
-            "world-.hello",
-            "world@hello.com",
-            "world_hello.com",
-            "httpWd",
-        ]
+            actual = Check().is_url_valid()
 
-        for domain in not_valid:
-            expected = False
+            self.assertEqual(expected, actual)
+
+            actual = Check(PyFunceble.CONFIGURATION["to_test"]).is_url_valid()
+
+            self.assertEqual(expected, actual)
+
+            del PyFunceble.CONFIGURATION["to_test"]
+
+    def test_is_url_valid_not_valid(self):
+        """
+        Test URL.is_url_valid() for the case that the URL is not valid.
+        """
+
+        expected = False
+
+        for domain in self.not_valid_domain:
             actual = Check().is_url_valid("https://%s/hello_world" % domain)
 
             self.assertEqual(expected, actual)
@@ -107,8 +174,13 @@ class TestCheck(TestCase):
 
             self.assertEqual(expected, actual)
 
-        # Test of the case that the protocol is not supported
-        for domain in not_valid:
+    def test_is_url_valid_protocol_not_supported(self):
+        """
+        Test URL.is_url_valid() for the case that the
+        URL protocol is not supported nor given.
+        """
+
+        for domain in self.not_valid_domain:
             expected = False
             actual = Check().is_url_valid("%s/hello_world" % domain)
 
@@ -118,91 +190,52 @@ class TestCheck(TestCase):
 
             self.assertEqual(expected, actual)
 
-    def test_is_domains_valid(self):
+    def test_is_domain_valid(self):
         """
-        This method test ExpirationDate().is_domain_valid().
+        Test ExpirationDate().is_domain_valid() for the case that domains
+        are valid.
         """
 
-        # Test of the case that the domains are valid
-        valid = [
-            "hello-world.com",
-            "hello.world.com",
-            "hello_world.world.com",
-            "hello_world.world.hello.com",
-            "hello.world_hello.world.com",
-            "hello---world.com",
-            "hello.world.hello.com",
-            "hello_.world.eu.com",
-            "_world.hello.eu.com",
-            "_world_.hello.eu.com",
-            "_hello-beautiful-world_.wold.eu.com",
-            "_hello_world_.hello.eu.com",
-            "xn--bittr-fsa6124c.com",
-            "xn--bllogram-g80d.com",
-            "xn--coinbse-30c.com",
-            "xn--cryptopi-ux0d.com",
-            "xn--cyptopia-4e0d.com",
-            "_hello.abuse.co.za",
-            "_hello_.abuse.co.za",
-            "_hello._world.abuse.co.za",
-            "_hello-world.abuse.co.za",
-            "_hello_world_.abuse.co.za",
-            "hello_world.abuse.co.za",
-            "hello-.abuse.co.za",
-        ]
         expected = True
 
-        for domain in valid:
+        for domain in self.valid_domain:
             PyFunceble.CONFIGURATION["to_test"] = domain
             actual = Check().is_domain_valid()
 
             self.assertEqual(expected, actual, msg="%s is invalid." % domain)
+
             actual = Check(PyFunceble.CONFIGURATION["to_test"]).is_domain_valid()
+
             self.assertEqual(expected, actual, msg="%s is invalid." % domain)
 
-        self.assertEqual(expected, actual)
+            del PyFunceble.CONFIGURATION["to_test"]
 
-        # Test of the case that the domains are not valid
-        not_valid = [
-            "hello-world",
-            "-hello.world",
-            "hello-.world",
-            "hello@world.com",
-            "hello_world.com",
-            "hello_world_.com",
-            "hello.com:443",
-            "world.hello:80",
-            "_world._hello.eu.com",
-            "_world.hello_.eu.com",
-            "bittréẋ.com",
-            "bịllogram.com",
-            "coinbȧse.com",
-            "cryptopiạ.com",
-            "cṙyptopia.com",
-            "-hello-world_all-mine_.hello.eu.com",
-            "test.-hello-world_all-mine_.abuse.co.za",
-            "-hello-world_.abuse.co.za",
-            "hello.-hello-world_.abuse.co.za",
-            "-hello.abuse.co.za",
-            "-hello-.abuse.co.za",
-        ]
+    def test_is_domain_valid_not_valid(self):
+        """
+        Test ExpirationDate().is_domain_valid() for the case that
+        we meet invalid domains.
+        """
+
         expected = False
 
-        for domain in not_valid:
+        for domain in self.not_valid_domain:
             PyFunceble.CONFIGURATION["to_test"] = domain
             actual = Check().is_domain_valid()
 
             self.assertEqual(expected, actual, msg="%s is valid." % domain)
+
             actual = Check(PyFunceble.CONFIGURATION["to_test"]).is_domain_valid()
 
             self.assertEqual(expected, actual, msg="%s is valid." % domain)
 
-    def test_is_subdomain(self):
+            del PyFunceble.CONFIGURATION["to_test"]
+
+    def test_is_subdomain_valid(self):
         """
-        This method test Check().is_subdomain().
+        Test Check().is_subdomain() for the case subdomains
+        are valid.
         """
 
-        # Test of the case that the domains are valid
         valid = [
             "hello_world.world.com",
             "hello_world.world.hello.com",
@@ -221,6 +254,7 @@ class TestCheck(TestCase):
             "hello_world.abuse.co.za",
             "hello-.abuse.co.za",
         ]
+
         expected = True
 
         for domain in valid:
@@ -228,14 +262,23 @@ class TestCheck(TestCase):
             actual = Check().is_subdomain()
 
             self.assertEqual(expected, actual, msg="%s is not a subdomain." % domain)
+
             actual = Check(PyFunceble.CONFIGURATION["to_test"]).is_subdomain()
+
             self.assertEqual(expected, actual, msg="%s is not a subdomain." % domain)
+
             actual = Check().is_subdomain(PyFunceble.CONFIGURATION["to_test"])
+
             self.assertEqual(expected, actual, msg="%s is not a subdomain." % domain)
 
-        self.assertEqual(expected, actual)
+            del PyFunceble.CONFIGURATION["to_test"]
 
-        # Test of the case that the domains are not valid
+    def test_is_subdomain_not_valid(self):
+        """
+        Test Check().is_subdomain() for the case subdomains
+        are not valid.
+        """
+
         not_valid = [
             "hello-world",
             "-hello.world",
@@ -249,6 +292,7 @@ class TestCheck(TestCase):
             "cṙyptopia.com",
             "google.com",
         ]
+
         expected = False
 
         for domain in not_valid:
@@ -256,17 +300,22 @@ class TestCheck(TestCase):
             actual = Check().is_subdomain()
 
             self.assertEqual(expected, actual, msg="%s is a subdomain." % domain)
+
             actual = Check(PyFunceble.CONFIGURATION["to_test"]).is_subdomain()
+
             self.assertEqual(expected, actual, msg="%s is a subdomain." % domain)
+
             actual = Check().is_subdomain(PyFunceble.CONFIGURATION["to_test"])
+
             self.assertEqual(expected, actual, msg="%s is a subdomain." % domain)
+
+            del PyFunceble.CONFIGURATION["to_test"]
 
     def test_is_ip_valid(self):
         """
-        This method test ExpirationDate().is_ip_valid().
+        Test ExpirationDate().is_ip_valid() for the case that the IP is valid.
         """
 
-        # test of the case that the ip is valid
         expected = True
         valid = ["15.47.85.65", "45.66.255.240"]
 
@@ -279,7 +328,12 @@ class TestCheck(TestCase):
 
             self.assertEqual(expected, actual, msg="%s is invalid." % ip_to_test)
 
-        # test of the case that the ip is invalid
+    def test_is_ip_valid_not_valid(self):
+        """
+        Test ExpirationDate().is_ip_valid() for the case that the IP
+        is not valid.
+        """
+
         expected = False
         invalid = ["google.com", "287.468.45.26", "245.85.69.17:8081"]
 
@@ -292,6 +346,8 @@ class TestCheck(TestCase):
             actual = Check(PyFunceble.CONFIGURATION["to_test"]).is_ip_valid()
 
             self.assertEqual(expected, actual, msg="%s is valid." % ip_to_test)
+
+            del PyFunceble.CONFIGURATION["to_test"]
 
 
 if __name__ == "__main__":
