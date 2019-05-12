8fd063aadae44134e495bdf37ece1e3bbbc3b1b0
funilrys <contact@funilrys.com>
2019-05-12 17:17:14 +0200

    Improvement of the way we generate the iana database file.
    Also:
      * Update of `iana-domains-db.json`

--- PyFunceble/__init__.py
+++ PyFunceble/__init__.py
@@ -93,7 +93,7 @@ from PyFunceble.whois import Whois
 # We set our project name.
 NAME = "PyFunceble"
 # We set out project version.
-VERSION = "1.37.0.detached-future (Blue Bontebok: Beetle)"
+VERSION = "1.38.0.detached-future (Blue Bontebok: Beetle)"
 
 # We set the list of windows "platforms"
 WINDOWS_PLATFORMS = ["windows", "cygwin", "cygwin_nt-10.0"]
