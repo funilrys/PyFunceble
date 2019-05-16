7c7f01d50aad16aafb9b3aba6f46113910e70c43
funilrys <contact@funilrys.com>
2019-05-16 20:44:40 +0200

    Deletion of uneeded line

--- PyFunceble/__init__.py
+++ PyFunceble/__init__.py
@@ -93,7 +93,7 @@ from PyFunceble.whois import Whois
 # We set our project name.
 NAME = "PyFunceble"
 # We set out project version.
-VERSION = "1.44.2.dev -- 2_0_0_rc3 -- (Blue Bontebok: Beetle)"
+VERSION = "1.44.3.dev -- 2_0_0_rc3 -- (Blue Bontebok: Beetle)"
 
 # We set the list of windows "platforms"
 WINDOWS_PLATFORMS = ["windows", "cygwin", "cygwin_nt-10.0"]
@@ -1314,7 +1314,6 @@ def _command_line():  # pragma: no cover pylint: disable=too-many-branches,too-m
                     DirectoryStructure()
 
                 if ARGS.dns:
-                    print(ARGS.dns)
                     CONFIGURATION.update({"dns_server": ARGS.dns})
 
                 if ARGS.execution:
