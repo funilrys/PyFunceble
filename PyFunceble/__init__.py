6575e0f3b4133958d86f81156789516c25a32eaa
funilrys <contact@funilrys.com>
2019-06-22 17:31:14 +0200

    Update iana-domains-db.json

--- PyFunceble/__init__.py
+++ PyFunceble/__init__.py
@@ -94,7 +94,7 @@ from PyFunceble.whois_lookup import WhoisLookup
 # We set our project name.
 NAME = "PyFunceble"
 # We set out project version.
-VERSION = "1.91.0.dev -- 2_0_0_rc36 -- (Blue Bontebok: Beetle)"
+VERSION = "1.91.1.dev -- 2_0_0_rc36 -- (Blue Bontebok: Beetle)"
 
 # We set the list of windows "platforms"
 WINDOWS_PLATFORMS = ["windows", "cygwin", "cygwin_nt-10.0"]
