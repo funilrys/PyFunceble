e74df4474662e5e1228636c6ae00773c55071c15
funilrys <contact@funilrys.com>
2019-11-01 01:09:08 +0100

    Introduction of more tests.

---
+++ tests/test_helpers_command.py
@@ -0,0 +1,115 @@
+# pylint:disable=line-too-long
+"""
+The tool to check the availability or syntax of domains, IPv4, IPv6 or URL.
+
+::
+
+
+    ██████╗ ██╗   ██╗███████╗██╗   ██╗███╗   ██╗ ██████╗███████╗██████╗ ██╗     ███████╗
+    ██╔══██╗╚██╗ ██╔╝██╔════╝██║   ██║████╗  ██║██╔════╝██╔════╝██╔══██╗██║     ██╔════╝
+    ██████╔╝ ╚████╔╝ █████╗  ██║   ██║██╔██╗ ██║██║     █████╗  ██████╔╝██║     █████╗
+    ██╔═══╝   ╚██╔╝  ██╔══╝  ██║   ██║██║╚██╗██║██║     ██╔══╝  ██╔══██╗██║     ██╔══╝
+    ██║        ██║   ██║     ╚██████╔╝██║ ╚████║╚██████╗███████╗██████╔╝███████╗███████╗
+    ╚═╝        ╚═╝   ╚═╝      ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝╚══════╝╚═════╝ ╚══════╝╚══════╝
+
+Tests of the PyFunceble.helpers.command
+
+Author:
+    Nissar Chababy, @funilrys, contactTATAfunilrysTODTODcom
+
+Special thanks:
+    https://pyfunceble.github.io/special-thanks.html
+
+Contributors:
+    https://pyfunceble.github.io/contributors.html
+
+Project link:
+    https://github.com/funilrys/PyFunceble
+
+Project documentation:
+    https://pyfunceble.readthedocs.io/en/dev/
+
+Project homepage:
+    https://pyfunceble.github.io/
+
+License:
+::
+
+
+    MIT License
+
+    Copyright (c) 2017, 2018, 2019 Nissar Chababy
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+"""
+# pylint: enable=line-too-long
+
+from os import environ
+from unittest import TestCase
+from unittest import main as launch_tests
+
+from PyFunceble.abstracts import Platform
+from PyFunceble.helpers import Command
+from stdout_base import StdoutBase
+
+
+class TestEnvironmentVariable(StdoutBase):
+    """
+    Tests of the PyFunceble.helpers.command.
+    """
+
+    def setUp(self):
+        """
+        Setups everything needed for the tests.
+        """
+
+        StdoutBase.setUp(self)
+
+    def tearDown(self):
+        """
+        Setups everything needed for after the tests.
+        """
+
+        StdoutBase.tearDown(self)
+
+    def test_execute(self):
+        """
+        Tests the execution of command.
+        """
+
+        if Platform.is_unix():
+            expected = "Hello, World!\n"
+            actual = Command("echo 'Hello, World!'").execute()
+
+            self.assertEqual(expected, actual)
+
+    def test_run(self):
+        """
+        Tests the run of a command.
+        """
+
+        if Platform.is_unix():
+            expected = ["Hello, World!"]
+            actual = list(Command("echo 'Hello, World!'").run())
+
+            self.assertEqual(expected, actual)
+
+
+if __name__ == "__main__":
+    launch_tests()
