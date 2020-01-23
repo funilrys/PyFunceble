19bf08d58b2ee219da9cdacaa894e6b5874f9be5
funilrys <contact@funilrys.com>
2020-01-23 23:55:17 +0100

    Bump version to v2.55.0

--- PyFunceble/abstracts/package.py
+++ PyFunceble/abstracts/package.py
@@ -73,7 +73,7 @@ class Package:
     :type: str
     """
 
-    VERSION = "2.54.2.dev (Green Galago: Skitterbug)"
+    VERSION = "2.55.0.dev (Green Galago: Skitterbug)"
     """
     Sets the package version.
 
