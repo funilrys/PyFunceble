e2d68acea454860682bb48606467b7d61b36f91b
funilrys <contact@funilrys.com>
2021-01-03 23:53:03 +0100

    Ensure that the CSV format is used.

--- .travis/complementary/custom_configuration.py
+++ .travis/complementary/custom_configuration.py
@@ -7,11 +7,14 @@ import PyFunceble.facility
 from PyFunceble import DomainAvailabilityChecker
 
 # We preset the indexes (from .PyFunceble.yaml) that we want to update.
-CUSTOM_CONFIGURATION_INDEX_VALUE_TO_SET = {"lookup": {"whois": False, "dns": False}}
+CUSTOM_CONFIGURATION_INDEX_VALUE_TO_SET = {
+    "lookup": {"whois": False, "dns": False},
+    "cli_testing": {"db_type": "csv"},
+}
 
 # We parse our custom indexes to PyFunceble before starting to use it.
-PyFunceble.facility.ConfigLoader.custom_config = CUSTOM_CONFIGURATION_INDEX_VALUE_TO_SET
 PyFunceble.facility.ConfigLoader.start()
+PyFunceble.facility.ConfigLoader.custom_config = CUSTOM_CONFIGURATION_INDEX_VALUE_TO_SET
 
 # From now, each call of test so in this example PyFuncebleTest,
 # will not try to get/request the WHOIS record.
