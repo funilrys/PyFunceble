8e3caae75f298eaa5f3c94cc7e9d47d8a798f30d
funilrys <contact@funilrys.com>
2021-01-07 00:26:57 +0100

    Update LICENSE.

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ from setup import get_version
 # -- Project information -----------------------------------------------------
 
 project = "PyFunceble"
-copyright = "2017, 2018, 2019, 2020 Nissar Chababy (@funilrys)"
+copyright = "2017, 2018, 2019, 2020, 2021 Nissar Chababy (@funilrys)"
 author = "Nissar Chababy (@funilrys)"
 
 # The short X.Y version
