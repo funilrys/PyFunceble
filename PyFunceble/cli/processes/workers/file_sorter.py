3ba14f98ba66aed7ce5790ac6fed383e269cae21
funilrys <contact@funilrys.com>
2021-01-16 02:03:44 +0100

    Improvement of the file sorter.
    Indeed, before this patch, we were splitting the files with small
    packages of 1000 lines. As of now, it will be 30_000 lines.
    
    This patch touches/fixes #190.
    
    Contributors:
      * @spirillen
      * @mitchellkrogza
      * @ZeroDot1

--- PyFunceble/cli/processes/workers/file_sorter.py
+++ PyFunceble/cli/processes/workers/file_sorter.py
@@ -50,6 +50,7 @@ License:
     limitations under the License.
 """
 
+import concurrent
 import contextlib
 import heapq
 import os
@@ -77,7 +78,7 @@ class FileSorterWorker(WorkerBase):
     """
 
     STD_NAME: str = "pyfunceble_file_sorter_worker"
-    MAX_LINES: int = 1000
+    MAX_LINES: int = 30_000
 
     def __post_init__(self) -> None:
         # We don't need to wait for anything here :-)
@@ -126,7 +127,8 @@ class FileSorterWorker(WorkerBase):
 
         return result
 
-    def process_file_sorting(self, file: str) -> None:
+    @classmethod
+    def process_file_sorting(cls, file: str) -> None:
         """
         Process the sorting of the given file.
 
@@ -145,7 +147,7 @@ class FileSorterWorker(WorkerBase):
             file_finished = False
 
             while True:
-                for _ in range(self.MAX_LINES + 1):
+                for _ in range(cls.MAX_LINES + 1):
                     try:
                         next_line = next(file_stream).strip()
                     except StopIteration:
@@ -202,9 +204,13 @@ class FileSorterWorker(WorkerBase):
         # Just for human brain :-)
         output_dir = FilesystemDirBase(consumed["destination"]).get_output_basedir()
 
-        files_to_sort = self.get_files_to_sort(output_dir)
+        with concurrent.futures.ThreadPoolExecutor(
+            max_workers=PyFunceble.storage.CONFIGURATION.cli_testing.max_workers,
+        ) as executor:
+            submitted_list = []
 
-        for file in files_to_sort:
-            self.process_file_sorting(file)
+            for file in self.get_files_to_sort(output_dir):
+                submitted = executor.submit(self.process_file_sorting, file)
+                submitted_list.append(submitted)
 
         return None
