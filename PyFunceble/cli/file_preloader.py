2b7de31b5e2f3c8decc813e1cda6424780d85e75
funilrys <contact@funilrys.com>
2021-02-21 21:01:51 +0100

    Fix issue with preloading.
    This patch fixes #223.
    
    Indeed, before this patch, I switched to the wrong key.

--- PyFunceble/cli/file_preloader.py
+++ PyFunceble/cli/file_preloader.py
@@ -334,7 +334,7 @@ class FilePreloader:
         self.__load_description()
 
         broken = False
-        file_helper = FileHelper(self.protocol["source"])
+        file_helper = FileHelper(self.protocol["subject"])
         self.__description["hash"] = HashHelper().hash_file(file_helper.path)
 
         if isinstance(self.continue_dataset, CSVContinueDataset):
