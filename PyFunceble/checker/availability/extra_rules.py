16c867d4af08baf922e3138a81dd3c685651a63a
funilrys <contact@funilrys.com>
2021-07-09 17:51:44 +0200

    Bump version to v4.0.0b62.

--- PyFunceble/checker/availability/extra_rules.py
+++ PyFunceble/checker/availability/extra_rules.py
@@ -96,9 +96,7 @@ class ExtraRulesHandler:
             r"\.tumblr\.com$": [(self.__switch_to_down_if, 404)],
             r"\.wix\.com$": [(self.__switch_to_down_if, 404)],
             r"\.wordpress\.com$": [self.__handle_wordpress_dot_com],
-            r"\.weebly\.com$": [
-                (self.__switch_to_down_if, 404)
-            ],
+            r"\.weebly\.com$": [(self.__switch_to_down_if, 404)],
         }
 
         if PyFunceble.facility.ConfigLoader.is_already_loaded():
