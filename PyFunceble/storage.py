8484911022628632c2c19f3f3c6bf4ae6b9a3cb8
funilrys <contact@funilrys.com>
2025-07-06 13:41:23 +0200

    Bump version to v4.3.0a24.dev

--- PyFunceble/storage.py
+++ PyFunceble/storage.py
@@ -60,7 +60,7 @@ from dotenv import load_dotenv
 from PyFunceble.storage_facility import get_config_directory
 
 PROJECT_NAME: str = "PyFunceble"
-PROJECT_VERSION: str = "4.3.0a23.dev (Blue Duckling: Tulip)"
+PROJECT_VERSION: str = "4.3.0a24.dev (Blue Duckling: Tulip)"
 
 DISTRIBUTED_CONFIGURATION_FILENAME: str = ".PyFunceble_production.yaml"
 
